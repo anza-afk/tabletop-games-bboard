1638438cb7a0ac85ae1bcee39dfd94058f9c7d84
Никита Кучин <varlok009@mail.ru>
2022-03-05 21:55:14 +0300

    кринж парсер и динамический шаблон

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
-from re import T
+
 from flask import Flask, render_template
+from test_parser import result_news
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     title = 'Поиск напарников для настольных игр'
-    return render_template('index.html', page_title=title)
+    return render_template('index.html', page_title=title, list_news=result_news)
 
 if __name__ == "__main__":
     app.run(debug=True)
