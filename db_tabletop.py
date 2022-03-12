ebed35020f831d30ae1ba6ef589eaee3164391d3
Anatoly Zapravadov <zapokami@gmail.com>
2022-03-12 17:07:06 +0300

    refactoring

--- db_tabletop.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from db_keys import DB
-engine = create_engine(DB)
-db_session = scoped_session(sessionmaker(bind=engine))
-
-Base = declarative_base()
-Base.query = db_session.query_property()
