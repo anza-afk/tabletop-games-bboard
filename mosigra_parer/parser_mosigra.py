51676e9e6ef0c3dc1a3403945a6d2163ec6e741b
Anatoly Zapravadov <zapokami@gmail.com>
2022-03-14 19:06:03 +0300

    fixed

--- mosigra_parer/parser_mosigra.py
+++ mosigra_parer/parser_mosigra.py
@@ -3,23 +3,25 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
+URL = 'https://www.mosigra.ru/nastolnye-igry/?page={0}'
 
-def get_html(url):
+
+def get_html(url:str, session:requests.Session) -> str:
     try: 
-        result = requests.get(url)
+        result = session.get(url)
         result.raise_for_status()
         return result.text
     except(requests.RequestException, ValueError):
-        return False
+        return None
 
 
-def get_last_page(html):
+def get_last_page(html:str) -> int:
     soup = bs(html, 'html.parser')
     url = soup.find('a', class_='last')
     return int(re.split(r"=|&",str(url))[3])
 
 
-def get_urls(html):  
+def get_urls(html:str) -> list:  
     links = []  
     soup = bs(html, 'html.parser')
     games = soup.find('div', class_='products-container').findAll('article')
@@ -29,9 +31,9 @@ def get_urls(html):
 
 
 if __name__ == '__main__':
-    url = 'https://www.mosigra.ru/nastolnye-igry/?page='
-    for page in range(1, (get_last_page(get_html(url))+1)):
-        current_page = get_html(f'{url}{page}')
+    my_session = requests.Session()
+    for page in range(1, (get_last_page(get_html(URL,my_session))+1)):
+        current_page = get_html(URL.format(page))
         with open('links_list.txt', 'a') as f:
             for link in get_urls(current_page):
                 f.write(link+'\n')
