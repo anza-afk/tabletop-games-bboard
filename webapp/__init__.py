136a6f8b5fb7a617bf7caa38b84feaf5ca79bb5d
Anatoly Zapravadov <zapokami@gmail.com>
2022-03-16 12:14:44 +0300

    functional login added plus logout mechanism

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,25 +1,58 @@
-from cmath import pi
-import imp
+from crypt import methods
 from werkzeug.security import generate_password_hash
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash, url_for
+from flask_login import LoginManager, current_user, login_user, logout_user
 from news_parser.test_parser import result_news
 from webapp.forms import LoginForm, RegistrationForm
 from webapp.users import check_user, add_user
 from webapp.models import User
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+
     @app.route('/')
     def index():
         title = 'Поиск напарников для настольных игр'
         return render_template('index.html', page_title=title, list_news=result_news)
-    
+
+
     @app.route('/login')
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = 'Авторизация'
         login_form = LoginForm()
         return render_template('login.html', page_title = title, form = login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Успешный вход')
+                return redirect(url_for('index'))
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
 
     @app.route('/registration', methods=['POST', 'GET'])
     def registration():
--- webapp/forms.py
+++ webapp/forms.py
@@ -1,13 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class" : "form-control"})
-    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class" : "form-control"})
-    email = StringField('Электронная почта', validators=[DataRequired()], render_kw={"class" : "form-control"})
-    org_checkbox = BooleanField('Вы представитель организации?')  # Задел на будущее, пока используем логин, пароль, почту
-    submit = SubmitField('Войти', render_kw={"class" : "btn btn-primary"})
+    username = StringField(
+        'Имя пользователя',
+        validators=[DataRequired()],
+        render_kw={"class" : "form-control"}
+    )
+    password = PasswordField(
+        'Пароль',
+        validators=[DataRequired()],
+        render_kw={"class" : "form-control"}
+    )
+    submit = SubmitField(
+        'Войти',
+        render_kw={"class" : "btn btn-primary"}
+    )
 
 
 class RegistrationForm(FlaskForm):
