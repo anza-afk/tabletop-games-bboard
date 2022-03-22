ada09ac222f07c23e393da2e7b8b96b7a5c0f0cb
Anatoly Zapravadov <zpokami@gmail.com>
2022-03-22 12:05:44 +0300

    added context manager for session

--- webapp/users.py
+++ webapp/users.py
@@ -10,40 +10,41 @@ def add_user(new_user: User) -> bool:
     Возвращает результат записи.
     """
     try:
-        db_session.add(new_user)
-        db_session.commit()
-        db_session.close()
+        with db_session() as session:
+            session.add(new_user)
+            session.commit()
         return True
     except sqlalchemy.exc: #  sqlalchemy.exc не обрабатываются, нужно понять как обрабатывать
         return False
 
 
 def add_profile(new_profile: UserProfile) -> None:
-    db_session.add(new_profile)
-    db_session.commit()
-    db_session.close()
+    with db_session() as session:
+        session.add(new_profile)
+        session.commit()
+
 
 
 def update_profile(form, user_id) -> None:
-    profile = db_session.query(UserProfile).filter(UserProfile.owner_id == user_id.id).first()
-    profile_email = db_session.query(User).filter(User.email == user_id.email).first()
-    profile_email.email = form['email'].data
-    profile.owner_id = user_id.id,
-    profile.name = form['name'].data,
-    profile.surname = form['surname'].data,
-    profile.country = form['country'].data,
-    profile.city = form['city'].data,
-    profile.favorite_games = form['favorite_games'].data,
-    profile.desired_games = form['desired_games'].data,
-    profile.about_user = form['about_user'].data
-    print(profile.surname)
-    db_session.commit()
-    db_session.close()
+    with db_session() as session:    
+        profile = db_session.query(UserProfile).filter(UserProfile.owner_id == user_id.id).first()
+        profile_email = db_session.query(User).filter(User.email == user_id.email).first()
+        profile_email.email = form['email'].data
+        profile.owner_id = user_id.id,
+        profile.name = form['name'].data,
+        profile.surname = form['surname'].data,
+        profile.country = form['country'].data,
+        profile.city = form['city'].data,
+        profile.favorite_games = form['favorite_games'].data,
+        profile.desired_games = form['desired_games'].data,
+        profile.about_user = form['about_user'].data
+        print(profile.surname)
+        db_session.commit()
 
 
 def join_profile(user_id):
-    result = db_session.query(UserProfile).filter(UserProfile.owner_id == user_id).first()
-    db_session.close()
+    with db_session() as session:
+        result = db_session.query(UserProfile).filter(UserProfile.owner_id == user_id).first()
     return result
 
 
@@ -53,9 +54,9 @@ def add_meeting(new_meeting: Meeting) -> bool:
     Возвращает результат записи.
     """
     try:
-        db_session.add(new_meeting)
-        db_session.commit()
-        db_session.close()
+        with db_session() as session:
+            db_session.add(new_meeting)
+            db_session.commit()
         return True
     except sqlalchemy.exc: #  sqlalchemy.exc не обрабатываются, нужно понять как обрабатывать
         return False
