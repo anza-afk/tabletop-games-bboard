6a6dcb4e7e4590a602eff514284e4ec55e360956
Anatoly Zapravadov <zapokami@gmail.com>
2023-03-26 17:38:07 +0300

    config example

---
+++ webapp/config_example.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = '*******************************************************'
+GAMES_PER_PAGE = 5
+SQLALCHEMY_DATABASE_URI = 'postgresql://*******:**********************@*********.**.**********.***/*******'
